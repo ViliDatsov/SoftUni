f870d8617c541265d3de897c55c790ef52c7525b
ViliDatsov <36599447+ViliDatsov@users.noreply.github.com>
2018-02-18 18:00:13 +0200

    Create 04. Even Powers of 2.java

---
+++ Programming Basics/Java/10. Advanced Loops/04. Even Powers of 2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NumbersNto1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int n = Integer.parseInt(scanner.nextLine());
+		int number = 1;
+    
+		for (int i = 0; i <=n ; i+=2) {
+			System.out.println(number);
+			number*=4;
+		}
+	}
+}
