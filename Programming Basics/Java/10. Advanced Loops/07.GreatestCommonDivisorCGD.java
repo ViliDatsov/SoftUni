bcb7cfa1ab92939be0de6a82cf4b5d2ff6c057b5
ViliDatsov <36599447+ViliDatsov@users.noreply.github.com>
2018-02-18 21:26:42 +0200

    Create 07.GreatestCommonDivisorCGD.java

---
+++ Programming Basics/Java/10. Advanced Loops/07.GreatestCommonDivisorCGD.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class GreatestCommonDivisor {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = Integer.parseInt(scanner.nextLine());
+	 	int b = Integer.parseInt(scanner.nextLine());
+
+	 	while(b!=0){
+	 		int oldb = b;
+	 		b=a%b;
+	 		a=oldb;
+			System.out.println(a);
+		}
+	}
+}
